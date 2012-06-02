9ca469fbf2fb46e332ed8a36e4335156985f7c40
Russell Cardullo <russellcardullo@gmail.com>
2012-06-02 14:28:49 -0400

    Fix bad reference to redis.
    Fixed a bad reference where redis authentication was using the
    redis variable instead of client variable.

--- app/dataProvider.js
+++ app/dataProvider.js
@@ -4,7 +4,7 @@ if (process.env.REDISTOGO_URL) {
   var redisUrl = require('url').parse(process.env.REDISTOGO_URL);
   console.log('Connecting to remote redis server ' + redisUrl.hostname + ':' + redisUrl.port);
   var client = redis.createClient(redisUrl.port, redisUrl.hostname);
-  redis.auth(redisUrl.auth.split(':')[1]);
+  client.auth(redisUrl.auth.split(':')[1]);
 } else {
   console.log('Connecting to local redis server');
   var client = redis.createClient();
